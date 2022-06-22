b5df6088d68a3272f172e8bf29aa2ce30201e7a9
Michael Teguh Laksana <michaelteguhlaksana@gmail.com>
2022-06-22 16:51:40 +0800

    Added bash script for SITL testing

---
+++ run_sim_test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+start_sim () {
+	cd ~/ardupilot/ArduSub
+	sim_vehicle.py -j4 &
+	roslaunch mavros apm.launch fcu_url:=udp://0.0.0.0:14550 &
+}
+
+#Start roscore
+roscore &
+
+#Start Sim
+
+start_sim &
+
+rosrun testing1 src/SAUVC22/testing1.py &
+
+rostopic echo /mavros/local_position/odom/pose
+
